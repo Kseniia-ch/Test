e3084f4df8861eec398299e0b0ffbdf03979ed69
Ksuha <dklfjdfijdpkof@gmail.com>
2019-08-31 11:21:35 +0300

    Добавлено вызов функции Факт

--- Info.cs
+++ Info.cs
@@ -4,7 +4,8 @@ class Program
 {
     static void Main(string [] args)
     {
-        System.Console.WriteLine("Hello world");    
+        System.Console.WriteLine("Hello world");   
+        Console.WriteLine(Fact(0));
     }
 
     static int Fact(int n)
